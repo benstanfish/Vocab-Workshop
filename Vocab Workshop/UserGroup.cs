5dadab35c78f0084f72b26a3b090ce1cef85be0e
benstanfish <benstanfish@hotmail.com>
2020-09-26 15:48:55 -0700

    Refactor

---
+++ Vocab Workshop/UserGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace Vocab_Workshop
+{
+    [DataContract]
+    class UserGroup
+    {
+        [DataMember(Name = "Id")]
+        private Guid _id = Guid.NewGuid();
+        public readonly  List<UserProfile> users = new List<UserProfile>();
+
+        
+    }
+}
